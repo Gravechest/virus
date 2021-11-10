9b525145b12b2a0f4acffb92e1cc590c988b410b
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-11-10 17:10:52 +0100

    Create server.c

---
+++ server.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+
+WSADATA data;
+SOCKET sock;
+SOCKET client;
+SOCKADDR_IN adres;
+SOCKADDR_IN adres2;
+char rbuf[9999];
+char command[40];
+
+int main() {
+	WORD ver = MAKEWORD(2, 2);
+	WSAStartup(ver, &data);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+    adres.sin_family = AF_INET;
+	adres.sin_port = htons(7777);
+	adres.sin_addr.S_un.S_addr = INADDR_ANY;
+	bind(sock,(SOCKADDR*)&adres,sizeof(adres));
+	listen(sock,SOMAXCONN);
+	client = accept(sock,0,0);
+	recv(client,rbuf,sizeof(rbuf),0);
+	printf("%s",rbuf);
+	for(;;){
+		scanf("%s",command);
+		send(client,command,sizeof(command),0);
+	}
+}
